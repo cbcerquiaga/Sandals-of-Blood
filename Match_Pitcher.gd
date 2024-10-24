1f4a0fd3cf47904a399a829bb8bfa7f74e280727
cbcerquiaga <cbcerquiaga@me.com>
2024-10-24 15:31:33 -0400

    Pitcher Slides
    Now to make it throw!

--- Match_Pitcher.gd
+++ Match_Pitcher.gd
@@ -6,9 +6,10 @@ const matchManager = preload("MatchManager.gd")
 var pitch_state = 0 #0 rotate, 1 slide, 2 power
 var max_pitch_state = 2
 var max_rotation = 0.5
-var max_slide = 50
+var max_slide = 200
+var current_slide = 0
 var rotationSpeed = 0.1
-var slideSpeed = 2.0
+var slideSpeed = 200.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,22 +27,28 @@ func _input(ev):
 		if	Input.is_key_pressed(KEY_LEFT) && !Input.is_key_pressed(KEY_RIGHT):
 			if (pitch_state == 0 && rotation <= max_rotation):
 				rotation += rotationSpeed
-			elif (pitch_state == 1):
-				velocity.x = slideSpeed
+			elif (pitch_state == 1 && current_slide > 0-max_slide):
+				velocity.x = 0- slideSpeed
+				print(current_slide)
 			pass
 		elif Input.is_key_pressed(KEY_RIGHT) && !Input.is_key_pressed(KEY_LEFT):
 			if (pitch_state == 0 && rotation >= 0-max_rotation):
 				rotation -= rotationSpeed
-			elif (pitch_state == 1):
-				velocity.x = 0-slideSpeed
+			elif (pitch_state == 1 && current_slide < max_slide):
+				velocity.x = slideSpeed
 		elif Input.is_key_pressed(KEY_SPACE):
 			if (pitch_state < 2):
 				pitch_state += 1
 		else:
 			velocity.x = 0
-			pass
+	if (current_slide < 0-max_slide && velocity.x < 0):
+		velocity.x = 0
+	if (current_slide > max_slide && velocity.x > 0):
+		velocity.x = 0
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_and_slide()
+	current_slide = current_slide + (velocity.x * delta)
 	pass
