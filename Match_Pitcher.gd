1e0a676ca1fa2ee697afb526a50ea6c312d4f85d
cbcerquiaga <cbcerquiaga@me.com>
2024-10-25 15:04:34 -0400

    Working on charging up that throw

--- Match_Pitcher.gd
+++ Match_Pitcher.gd
@@ -10,6 +10,8 @@ var max_slide = 200
 var current_slide = 0
 var rotationSpeed = 0.1
 var slideSpeed = 200.0
+var pitcher_power = 100
+var ball_power = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -37,15 +39,45 @@ func _input(ev):
 			elif (pitch_state == 1 && current_slide < max_slide):
 				velocity.x = slideSpeed
 		elif Input.is_key_pressed(KEY_SPACE):
-			if (pitch_state < 2):
+			if (pitch_state < 2): #0 rotate, 1 slide
 				pitch_state += 1
+			if (pitch_state == 2): #2 power, #3 spin
+				#emit a signal for the charge-up bar
+				emit_signal("pitch_charge")
+				chargePitch()
+				pass
 		else:
 			velocity.x = 0
-	if (current_slide < 0-max_slide && velocity.x < 0):
-		velocity.x = 0
-	if (current_slide > max_slide && velocity.x > 0):
-		velocity.x = 0
+		if (current_slide < 0-max_slide && velocity.x < 0):
+			velocity.x = 0
+		if (current_slide > max_slide && velocity.x > 0):
+			velocity.x = 0
 		pass
+		
+		
+		
+func chargePitch():
+	var cooldown = 100
+	var increment = 10
+	var isGoingUp = true
+	while (!Input.is_key_pressed(KEY_B) && cooldown > 0):
+		print ("looping")
+		if (cooldown > 0):
+			cooldown = cooldown - 1
+		if (isGoingUp):
+			if (ball_power <= pitcher_power - increment):
+				ball_power = ball_power + increment
+			else:
+				isGoingUp = false
+				print ("max ball power")
+		else:
+			if (ball_power >= 0 + increment):
+				ball_power = ball_power - increment
+			else:
+				isGoingUp = true
+				print ("min ball power")
+	print("Throw power is " + str(ball_power)  + " and cooldown is " + str(cooldown))
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
