ec3a3fc92e892be095e905417778dd4bd787aa8c
cbcerquiaga <cbcerquiaga@me.com>
2025-07-29 21:45:10 -0400

    Started working on controller input
    #54 started some very early work on controller aim support

--- aim_target.gd
+++ aim_target.gd
@@ -2,10 +2,16 @@ extends Node2D
 class_name AimTarget
 
 @onready var on:bool = false
+@onready var controllerMode:bool = false
 
 func _process(delta: float) -> void:
-		if on:
+		if on and !controllerMode:
 			global_position = get_global_mouse_position()
+			visible = true
+		elif on and controllerMode:
 			visible = true
 		else:
 			visible = false
+		
+#TODO: input event mouse turns off controller mode
+#TODO: input event from controller turns on controller mode
