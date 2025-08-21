1c694ce90d3aeea0f8e7769a8e787342c0ac0175
cbcerquiaga <cbcerquiaga@me.com>
2025-08-21 17:37:38 -0400

    Imported sprites for the power bar
    #1 imported the assets and made pseudocode for a pitch aim/power/curve indicator

---
+++ powerbar.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var maxPower
+var currentPower
+var maxCurve
+var currentCurve
+var aimDirection
+
+
+#TODO: take power value from pitcher
+#TODO: as power goes up, spread the segments out
+#TODO: if power is less than 33% of max, switch to green textures; if curve is more than 66% of max, switch to orange
+#TODO: if curve is not zero, tilt the segments and bow out the middle one
