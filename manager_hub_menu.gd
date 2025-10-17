41ee65e5ef505c0605fce4ead07c5e4d9b2df0d2
cbcerquiaga <cbcerquiaga@me.com>
2025-10-17 14:01:22 -0400

    Started work on main hub menu
    #141 made the scene hierarchy and started working on the script

---
+++ manager_hub_menu.gd
@@ -0,0 +1,50 @@
+extends Control
+
+@onready var game_today: bool = true
+@onready var today_button: TextureButton = $"HBoxContainer/Travel-GameContainer/TextureButton"
+
+func travelDay():
+	game_today = false
+	today_button.texture_normal = load("res://UI/HubUI/TravelDay_button_base.png")
+	today_button.texture_focused = load("res://UI/HubUI/TravelDay_button_highlighted.png")
+	today_button.texture_hover = load("res://UI/HubUI/TravelDay_button_highlighted.png")
+	#TODO: switch popup menu to travel
+	pass
+
+func gameDay():
+	game_today = true
+	today_button.texture_normal = load("res://UI/HubUI/GameDay_button_base.png")
+	today_button.texture_focused = load("res://UI/HubUI/GameDay_button_highlighted.png")
+	today_button.texture_hover = load("res://UI/HubUI/GameDay_button_highlighted.png")
+	#TODO: switch popup menu to game
+	pass
+
+
+func _on_system_focus() -> void:
+	pass # Replace with function body.
+
+
+func _on_career_focus() -> void:
+	pass # Replace with function body.
+
+
+func _on_league_focus() -> void:
+	pass # Replace with function body.
+
+
+func _on_management_focus() -> void:
+	pass # Replace with function body.
+
+
+func _on_team_focused() -> void:
+	pass # Replace with function body.
+
+
+func _on_today_focus() -> void:
+	if game_today:
+		#TODO: pop up game related popup
+		pass
+	else:
+		#TODO: pop up travel related popup
+		pass
+	pass # Replace with function body.
