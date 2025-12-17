0b422741f8c4ecc4298a420510aff52c6dd5d33b
cbcerquiaga <cbcerquiaga@me.com>
2025-12-16 20:38:42 -0500

    Added WorldCities global singleton
    #187 closed. This hardcoded object will hold all of the city data in one place for easy reference in every file

---
+++ WorldCities.gd
@@ -0,0 +1,59 @@
+extends Node
+#used as a singleton called Cities. Stores all cities in the game
+
+#TODO: coordinates
+#TODO: inout city info
+
+var newBeach: City #rising sea levels have made a previously inland small town into the beach
+
+var henrytown: City #a monument to John Henry stands in this old railway village
+
+var delta: City #the mouth of the new river
+
+var craterCity: City #some kind of explosion has left a large area ripe for development
+
+var discountAuto: City #small town centered around the discount auto parts shop
+
+var ironHills: City #town built on the cliffside of the iron hills
+
+var skyway: City #built from an old airport
+
+var hoa: City #gated community turned town
+
+var campus: City #community college turned town
+
+var soulCity: City #musical town, upriver from Delta
+
+var raceway: City #centered around an old racetrack
+
+var dogtown: City #remnants of the old industrual world, rebuilt into something new
+
+var picketville: City #an old wealthy neighborhood
+
+var amberfield: City #expanses of arable land
+
+var ranchoGringo: City #once the home of preppers, not anymore
+
+var corbusier: City #giant tower surrounded by open land
+
+var towerForest: City #old downtown of a major city
+
+var portlandia: City #sea level rise led to a shipping warehouse becoming a perfect loading dock
+
+var blueSwamp: City #misnomer, the water is quite brown
+
+var brownSwamp: City #misnomer, actually quite nice lake town
+
+var hauntedLake: City #in the old times, there was a settlement here, but it was flooded
+
+var concretia: City #sea level rise flooded the city, but its massive overpasses staied above the water, perfect for a fishing town
+
+var badlands: City #desert oasis
+
+var rigCity: City #oil rig washed ashore in a storm
+
+var oceania: City #downtown aquarium turned city
+
+var cahokia: City #ancient mound building area
+
+var san_myra: City #old Spanish fort
