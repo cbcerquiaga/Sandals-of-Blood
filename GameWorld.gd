2e8e202f3c21a93936b83d072e3bf37ef2f604cd
cbcerquiaga <cbcerquiaga@me.com>
2026-01-03 16:25:14 -0500

    Progress on pro/rel
    #181 Added more code regarding promotion and relegation

--- GameWorld.gd
+++ GameWorld.gd
@@ -1,19 +1,176 @@
 extends Node
 
-var leagues= [] #TODO: create League objects
-var year: int = 2245
+var leagues= []
+var year: int = 2160
 var week: int = 40 #after 52, advance one year
+
+var all_amer_promo_playoff: = [] #week, team 1, team 2, hosting team (venue)
+var double_a_promo_playoff: = []
+var professional_promo_playoff: = []
+var all_amer_winner: Franchise #one team wins the all_amer_promo_playoff
+var double_a_winner_1: Franchise #two teams win the double_a_promo_playoff
+var double_a_winner_2: Franchise
+var pro_winner_1: Franchise #three teams win the professional_promo_plaoyff
+var pro_winner_2: Franchise
+var pro_winner_3: Franchise
 
 var max_world_contract_value = 110000 #the contract value of the highest paid player in the world now
 
-var records_career #TODO: create a dictionary of stats and store the character object and the stat
+var records_career := {
+	"gp": [], #record, character: NPC
+	"starts": [],
+	"goals": [], 
+	"assists": [],
+	"points": [],
+	"sacks": [],
+	"partner_sacks": [],
+	"hits": [],
+	"knockouts": [],
+	"aces": [],
+	"pitches_thrown": [],
+	"faceoff_wins": [],
+	"faceoff_diff": [],
+	"guard_rating": [],
+	"pressure_rating": [],
+	"involvement_rating": [],
+	"attack_rating": [],
+	"diff_per_play": [],
+	"ace_rate": [],
+	"clean_sheets": [],
+	"shutouts": [],
+	"returns": [],
+	"hattricks": [],
+	"5-hits":  [],
+	"goal_diff": [],
+	"coach_games": [],
+	"goach_wins": []
+}
 
-var records_season
+var records_season := {
+	"goals": [], #record, character: NPC
+	"assists": [],
+	"points": [],
+	"sacks": [],
+	"partner_sacks": [],
+	"hits": [],
+	"knockouts": [],
+	"aces": [],
+	"pitches_thrown": [],
+	"faceoff_wins": [],
+	"faceoff_diff": [],
+	"guard_rating": [],
+	"pressure_rating": [],
+	"involvement_rating": [],
+	"attack_rating": [],
+	"diff_per_play": [],
+	"ace_rate": [],
+	"clean_sheets": [],
+	"shutouts": [],
+	"returns": [],
+	"hattricks": [],
+	"5-hits":  [],
+	"goal_diff": []
+}
 
-var records_game
+var records_game := {
+	"goals": [], #record, character: NPC, year: int
+	"assists": [],
+	"sacks": [],
+	"partner_sacks": [],
+	"hits": [],
+	"knockouts": [],
+	"aces": [],
+	"pitches_thrown": [],
+	"faceoff_wins": []
+}
 
 
 func advance_week():
 	for league in leagues:
 		league.advance_week(week)
 	CareerFranchise.determine_random_events()
+	if week == 30: #championships have happened, now for promotion/relegation
+		design_promo_playoffs()
+		#TODO: first round of pro/rel playoffs
+	elif week == 31:
+		update_playoffs_round_one() # update all of the playoffs based on results from first round
+		#TODO: second round of pro/rel playoffs
+		pass
+	elif week == 32:
+		update_playoffs_round_two() # update all of the playoffs based on results from second round
+		#TODO: last chance dance! third round of pro/rel playoffs
+		pass
+	elif week == 33:
+		promote_and_relegate()
+	elif week == 52:
+		week = 0
+		year = year + 1
+		
+
+func design_promo_playoffs():
+	#runner ups in badlands and maritime league play each other, winner faces seed 6 from all-american league
+	all_amer_promo_playoff[0] = [30, leagues[1].promotion_playoff_team, leagues[2].promotion_playoff_team, leagues[0].demotion_playoff_team] #all games take place at all_american league seed 6's home arena
+	all_amer_promo_playoff[1] = [31, leagues[0].demotion_playoff_team, null, leagues[0].demotion_playoff_team] #winner from week 1 plays all-american seed 6, winner goes up
+	
+	#runner ups in metropolitan, palmetto, and iron leagues  face seed 7 from maritime and badlands
+	var a_teams = [leagues[3].promotion_playoff_team, leagues[4].promotion_playoff_team, leagues[5].promotion_playoff_team]
+	#TODO: sort a_teams by standings: points, wins, goal difference, last year position
+	var aa_teams = [leagues[1].demotion_playoff_team, leagues[2].demotion_playoff_team]
+	double_a_promo_playoff[0] = [30, a_teams[1], aa_teams[0], aa_teams[0]] #round 1 a
+	double_a_promo_playoff[1] = [30, a_teams[2], aa_teams[2], aa_teams[2]] #round 1 aa
+	double_a_promo_playoff[2] = [31, a_teams[0], null, a_teams[0]] # round 1 a winner faces a[0] who has a bye
+	double_a_promo_playoff[3] = [32, null, null, null] #round 2 a winner faces aa winner, hosted by aa winner
+	
+	var fringe_teams = [leagues[3].demotion_playoff_team, leagues[4].demotion_playoff_team, leagues[5].demotion_playoff_team]
+	#TODO: sort fringe_teams by standings
+	var b_teams = [leagues[6].promotion_playoff_team, leagues[7].promotion_playoff_team]
+	professional_promo_playoff[0] = [30, b_teams[0], b_teams[1], fringe_teams[0]] #hosted by the team they have to play if they win
+	professional_promo_playoff[1] = [30, fringe_teams[1], fringe_teams[2], fringe_teams[1]] #winner becomes pro_winner_1
+	professional_promo_playoff[2] = [31, null, fringe_teams[0], fringe_teams[0]] #winning b team faces best fringe team; winner becomes pro_winner_2
+	professional_promo_playoff[3] = [32, null, null, null] #losers from [1] and [2] play each other, winner becomes pro_winner_3
+	
+func update_playoffs_round_one():
+	#TODO
+	pass
+	
+func update_playoffs_round_two():
+	#TODO
+	pass
+	
+func promote_and_relegate():
+	# AAA: the show
+	var all_amer = leagues[0]
+	var aaa_down = [] #moving down from aaa to aa #TODO: append all the teams in all_amer_promo_playoff who are not all_amer_winner, append all_amer.demotion_teams
+	var aaa_up = [leagues[1].champion, leagues[2].champion, all_amer_winner] #moving up from aa to aaa
+	#TODO: replace aaa_down with aaa_up in teams and standings for all_amer using all_amer.replace_team
+	
+	# AA: fully pro
+	var maritime = leagues[1]
+	var badlands = leagues[2]
+	var aa_down = [] #moving down from aa to a #TODO: add maritime.demotion_teams, badlands.demotion_teams, and every team from double_a_promo_playoff who is not a winner
+	var aa_up = [double_a_winner_1, double_a_winner_2, leagues[3].champion, leagues[4].champion, leagues[5].champion] #moving up from a to aa
+	var going_aa #TODO: append all elements in aaa_down and aa_up
+	for franchise in going_aa:
+		var point = franchise.city.map_location
+		var dist_maritime = point.distance_squared_to(maritime.hub_point)
+		var dist_badland = point.distance_squared_to(badlands.hub_point)
+	#TODO: identify which teams from going_aa are closer to maritime and which are closer to badlands, reduce total travel distance when assigning teams to each league
+	
+	
+	# A: semi-pro
+	var metro = leagues[3]
+	var palmetto = leagues[4]
+	var iron = leagues[5]
+	var a_down = [] #moving down from a to b
+	var a_up = [] #moving up from b to a
+	var going_a
+	for franchise in going_a:
+		var point = franchise.city.map_location
+		var dist_metro =  point.distance_squared_to(metro.hub_point)
+		var dist_palmetto = point.distance_squared_to(palmetto.hub_point)
+		var dist_iron = point.distance_squared_to(iron.hub_point)
+		
+	# B: amateur
+	var bush = leagues[6]
+	var suel = leagues[7]
+	pass
--- league.gd
+++ league.gd
@@ -6,14 +6,16 @@ var league_abbreviation
 var league_level #B, A, AA, or AAA
 var hub_point: Vector2
 var teams:= [] #array of Franchises
+var standings = [] #sorted teams
+var referees:= [] #array of Referees
+var referees_used_today := []
 var season_length: int = 28
 var promoting_league: League
 var demoting_league: League
 var num_playoff_champ: int = 2 #how many teams are involved in the championship playoff, if 1 top team immediately wins
-var num_playoff_demote: int = 1
+var num_playoff_demote: int = 1 #how many teams go into the playoff to determine if they get relegated
 var num_promoted_playoff: int = 1 #how many teams go into the playoff for promotion
-var num_demoted_playoff: int = 2 #from relegation playoff
-var num_promoted_auto: int = 1 #from regular season position
+var champ_promotes_auto: bool = true #if the champion automatically goes up to the next league	
 var num_demoted_auto: int = 2 #from regular season position
 var league_dues: int = 450
 var cash_prize: int = 800
@@ -26,16 +28,86 @@ var promotion_playoff_done: bool = false
 var champion: Franchise
 var promotion_playoff_team: Franchise
 var demotion_teams:= []
-var demotion_playoff_teams:= []
+var demotion_playoff_team: Franchise
+
+var records_career := {
+	"gp": [], #record, character: NPC
+	"starts": [],
+	"goals": [], 
+	"assists": [],
+	"points": [],
+	"sacks": [],
+	"partner_sacks": [],
+	"hits": [],
+	"knockouts": [],
+	"aces": [],
+	"pitches_thrown": [],
+	"faceoff_wins": [],
+	"faceoff_diff": [],
+	"guard_rating": [],
+	"pressure_rating": [],
+	"involvement_rating": [],
+	"attack_rating": [],
+	"diff_per_play": [],
+	"ace_rate": [],
+	"clean_sheets": [],
+	"shutouts": [],
+	"returns": [],
+	"hattricks": [],
+	"5-hits":  [],
+	"goal_diff": [],
+	"coach_games": [],
+	"goach_wins": []
+}
+
+var records_season := {
+	"goals": [], #record, character: NPC
+	"assists": [],
+	"points": [],
+	"sacks": [],
+	"partner_sacks": [],
+	"hits": [],
+	"knockouts": [],
+	"aces": [],
+	"pitches_thrown": [],
+	"faceoff_wins": [],
+	"faceoff_diff": [],
+	"guard_rating": [],
+	"pressure_rating": [],
+	"involvement_rating": [],
+	"attack_rating": [],
+	"diff_per_play": [],
+	"ace_rate": [],
+	"clean_sheets": [],
+	"shutouts": [],
+	"returns": [],
+	"hattricks": [],
+	"5-hits":  [],
+	"goal_diff": []
+}
+
+var records_game := {
+	"goals": [], #record, character: NPC, year: int
+	"assists": [],
+	"sacks": [],
+	"partner_sacks": [],
+	"hits": [],
+	"knockouts": [],
+	"aces": [],
+	"pitches_thrown": [],
+	"faceoff_wins": []
+}
+
 
 func generate_season_plan():
 	#TODO: randomly generate a season
-	#each team plays each other team 4 times, 2 at home and 2 away
+	#each team plays each other team 4 times: 2 at home and 2 away
 	#every team gets a break at week 15 (after playing each team 2x)
 	#assign values into season_plan
 	pass
 
 func advance_week(week: int):
+	referees_used_today = []
 	for game in season_plan:
 		if game[0] == week:
 			if game[1] == CareerFranchise:
@@ -51,9 +123,17 @@ func advance_week(week: int):
 			playoff_time = false
 			break
 		if playoff_time:
-			determine_playoff_seeding()
+			determine_champ_seeding()
+			assign_relegation_teams()
+	if is_offseason:
+		sim_cpu_tryouts()
+		pass
+	if !roster_freeze:
+		sim_cpu_signings()
 				
 func human_match(cpu_team: Franchise, isHomeTeam: bool, isPlayoffs: bool = false):
+	var ref = pick_referee()
+	referees_used_today.append(ref)
 	pass
 	
 func sim_match(homeTeam: Franchise, awayTeam: Franchise):
@@ -62,3 +142,54 @@ func sim_match(homeTeam: Franchise, awayTeam: Franchise):
 func sim_cpu_signings():
 	pass
 	
+func sim_cpu_tryouts():
+	pass
+	
+func determine_champ_seeding():
+	sort_standings()
+	if num_playoff_champ == 1:
+		var top_team = standings[1]
+		champion = top_team
+		promotion_playoff_team = top_team
+	else: #always 2
+		var top_seed = standings[1]
+		var two_seed = standings[2]
+		pass
+
+func assign_relegation_teams():
+	sort_standings()
+	if num_demoted_auto == 2:
+		demotion_teams.append(standings[8])
+		demotion_teams.append(standings[7])
+		demotion_playoff_team = standings[6]
+	elif num_demoted_auto == 1:
+		demotion_teams.append(standings[8])
+		demotion_playoff_team = standings[7]
+	else: #no teams demoted automatically
+		demotion_playoff_team = standings[8]
+	
+func pick_referee():
+	var available_referees
+	if referees_used_today.size() == 0:
+		available_referees = referees
+	else:
+		for referee in referees:
+			if referees_used_today.find(referee) == -1:
+				available_referees.append(referee)
+	var rand = randi_range(0, available_referees.size())
+	return available_referees[rand]
+	
+func sort_standings():
+	for team in teams:
+		var points = team.get_standings_points() #2 points for a win, 1 for a tiw, 0 for a loss
+		var wins = team.wins #if two teams had the same points but one had more wins, they go higher
+		var goal_diff = team.goal_diff #if two teams are otherwise tied, goal differential decides
+		var last_year = team.last_season_position #between 1 and 10 (1-8 this legue, 9 and 10 promoted from lower league), tie goes to team with worse position last year
+	standings #TODO: fill with teams and sort
+	pass
+	
+func replace_team(old_team: Franchise, new_team: Franchise):
+	var teams_index = teams.find(old_team)
+	teams[teams_index] = new_team
+	var standings_index = standings.find(old_team)
+	standings[standings_index] = new_team
